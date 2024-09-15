6ac7b472bd05e8945affb38ae006027dae0387f3
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-09-15 17:59:10 +0500

    Add project files.

---
+++ Tyuiu.KurbanovFA.Sprint1.Task0.V24.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.KurbanovFA.Sprint1.Task0.V24.Lib;
+
+namespace Tyuiu.KurbanovFA.Sprint1.Task0.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+        }
+    }
+}
