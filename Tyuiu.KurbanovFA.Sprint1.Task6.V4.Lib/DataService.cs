72aef3fdf1e24e6e473cc47371d432d615aaf9a7
Tyuiu.KurbanovFA@outlook.com <Tyuiu.KurbanovFA@outlook.com>
2024-09-17 02:48:47 +0500

    Task6

--- Tyuiu.KurbanovFA.Sprint1.Task6.V4.Lib/DataService.cs
+++ Tyuiu.KurbanovFA.Sprint1.Task6.V4.Lib/DataService.cs
@@ -6,9 +6,21 @@ namespace Tyuiu.KurbanovFA.Sprint1.Task6.V4.Lib
     {
         public string CheckDoubleN(string value)
         {
-            if (value.Contains("нн"))
+            string s, s1;
+            s1 = "";
+            for (int i = 0; i < value.Length; i++)
             {
-                value = "";
+                if (value[i] != ' ')
+                {
+                    s1 += value[i];
+                }
+                else
+                {
+                    if (s1.Contains("нн"))
+                    {
+                        s1 = "";
+                    }
+                }
             }
             return value;
         }
